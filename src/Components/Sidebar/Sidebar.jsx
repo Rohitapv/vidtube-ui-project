9b4147cd6b361ca265d6d87bca7fb93c0c2b3739
Rohitapv <rohitdad2001@gmail.com>
2024-04-07 11:32:13 +0530

    Revert "Added sidebar changes"
    This reverts commit 014668a6605d992ad2835fe89e3b2617138fa6cc.

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,54 +1,50 @@
-import React from 'react'
-import './Sidebar.css'
-import home from '../../assets/home.png'
-import game_icon from '../../assets/game_icon.png'
-import automobiles from '../../assets/automobiles.png'
-import sports from '../../assets/sports.png'
-import entertainment from '../../assets/entertainment.png'
-import tech from '../../assets/tech.png'
-import music from '../../assets/music.png'
-import blogs from '../../assets/blogs.png'
-import news from '../../assets/news.png'
-import jack from '../../assets/jack.png'
-import simon from '../../assets/simon.png'
-import tom from '../../assets/tom.png'
-import megan from '../../assets/megan.png'
-import cameron from '../../assets/cameron.png'
-const Sidebar = () => {
-    return (
-        <div className='sidebar'>
-            <div className="sortcut-links">
-                <div className="sidelinks">
-                    <img src={home} alt=""/><p>Home</p>
-                </div>
-                <div className="sidelinks">
-                    <img src={game_icon} alt=""/><p>game_icon</p>
-                </div>
-                <div className="sidelinks">
-                    <img src={automobiles} alt=""/><p>automobiles</p>
-                </div>
-                <div className="sidelinks">
-                    <img src={sports} alt=""/><p>sports</p>
-                </div>
-                <div className="sidelinks">
-                    <img src={entertainment} alt=""/><p>entertainment</p>
-                </div>
-                <div className="sidelinks">
-                    <img src={tech} alt=""/><p>tech</p>
-                </div>
-                <div className="sidelinks">
-                    <img src={music} alt=""/><p>music</p>
-                </div>
-                <div className="sidelinks">
-                    <img src={blogs} alt=""/><p>blogs</p>
-                </div>
-                <div className="sidelinks">
-                    <img src={news} alt=""/><p>news</p>
-                </div>
-                <hr/>
-            </div>
-        </div >
-    )
-}
+// import React from 'react'
+// import './Sidebar.css'
+// import home from '../../assets/home.png'
+// import game_icon from '../../assets/game_icon.png'
+// import automobiles from '../../assets/automobiles.png'
+// import sports from '../../assets/sports.png'
+// import entertainment from '../../assets/entertainment.png'
+// import tech from '../../assets/tech.png'
+// import music from '../../assets/music.png'
+// import blogs from '../../assets/blogs.png'
+// import news from '../../assets/news.png'
+// import jack from '../../assets/jack.png'
+// import simon from '../../assets/simon.png'
+// import tom from '../../assets/tom.png'
+// import megan from '../../assets/megan.png'
+// import cameron from '../../assets/cameron.png'
+// const Sidebar = () => {
+//     return (
+//         <div className='sidebar'>
+//             <div className="sortcut-links">
+//                 <div className="sidelinks">
+//                     <img src={home} alt=""/><p>Home</p>
+//                 </div>
+//                 <div className="sidelinks">
+//                     <img src={home} alt=""/><p>Home</p>
+//                 </div>
+//                 <div className="sidelinks">
+//                     <img src={home} alt=""/><p>Home</p>
+//                 </div>
+//                 <div className="sidelinks">
+//                     <img src={home} alt=""/><p>Home</p>
+//                 </div>
+//                 <div className="sidelinks">
+//                     <img src={home} alt=""/><p>Home</p>
+//                 </div>
+//                 <div className="sidelinks">
+//                     <img src={home} alt=""/><p>Home</p>
+//                 </div>
+//                 <div className="sidelinks">
+//                     <img src={home} alt=""/><p>Home</p>
+//                 </div>
+//                 <div className="sidelinks">
+//                     <img src={home} alt=""/><p>Home</p>
+//                 </div>
+//                 <hr/>
+//             </div>
+//         </div >
+// }
 
-export default Sidebar
+// export default Sidebar
