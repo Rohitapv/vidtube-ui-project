52b60adf20be52333d91e7b8b55ab951afb3840d
Rohitapv <rohitdad2001@gmail.com>
2024-04-07 11:59:19 +0530

    Added side bar changes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 // import {BrowserRouter as Router} from "react-router-dom"
 // import { Route, Routes} from "react-router-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home';
 const App = () => {
   return (
     <div>
-    {/* <Navbar/> */}
-    <BrowserRouter>
+     <Navbar/> 
+    {/* <BrowserRouter>
     <Routes>
      <Route path='/nav'element={<Navbar/>}/>
     </Routes>
-    </BrowserRouter>
-
+    </BrowserRouter> */}
+    <Home/>
   </div>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Home.css'
-import Sidebar from'./Components/Sidebar/Sidebar'
+import Sidebar from'../../Components/Sidebar/Sidebar'
 
 const Home = () => {
   return (
